07103c13e113d139b849e670b359c63f6ef8681d
MohammadJavad <57481105+MommadJavad@users.noreply.github.com>
2021-11-06 21:37:22 +0330

    Revert "session-2"

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,8 @@
-const path = require("path");
-
 module.exports = {
   entry: "./src/index.js",
   output: {
     filename: "bundle.js",
-    path: path.resolve(__dirname, "./dist"),
+    path: "./dist",
   },
   mode: "none",
 };
